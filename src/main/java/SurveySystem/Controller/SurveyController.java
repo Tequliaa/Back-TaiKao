ddebe0b15a844eacfe65c1709b1c33f814365fa1
bruceleezz <672472298@qq.com>
2025-04-17 23:26:24 +0800

    可视化构建问卷，初步实现。

--- src/main/java/SurveySystem/Controller/SurveyController.java
+++ src/main/java/SurveySystem/Controller/SurveyController.java
@@ -1,9 +1,15 @@
 package SurveySystem.Controller;
 
 
+import SurveySystem.Model.Option;
+import SurveySystem.Model.Question;
 import SurveySystem.Model.Result;
 import SurveySystem.Model.Survey;
+import SurveySystem.Model.SurveyDto;
+import SurveySystem.Service.OptionService;
+import SurveySystem.Service.QuestionService;
 import SurveySystem.Service.SurveyService;
+import lombok.Data;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -15,9 +21,14 @@ import java.util.Map;
 public class SurveyController {
 
     private final SurveyService surveyService;
-
-    public SurveyController(SurveyService surveyService) {
+    private final QuestionService questionService;
+    private final OptionService optionService;
+    public SurveyController(SurveyService surveyService,
+                            QuestionService questionService,
+                            OptionService optionService) {
         this.surveyService = surveyService;
+        this.questionService = questionService;
+        this.optionService = optionService;
     }
 
 
@@ -43,10 +54,85 @@ public class SurveyController {
         return Result.success(surveys);
     }
 
-    @GetMapping("/getSurveyById")
-    public Result<Survey> getSurveyById(@RequestParam int surveyId) {
+    @GetMapping("/getSurveyAndQuestionsById")
+    public Result<Map<String, Object>> getSurveyById(@RequestParam int surveyId) {
         Survey survey = surveyService.getSurveyById(surveyId);
-        return Result.success(survey);
+        List<Question> questions=questionService.getAllQuestions(surveyId);
+        for(Question question:questions){
+            List<Option> options= optionService.getOptionsByQuestionId(question.getQuestionId());
+            question.setOptions(options);
+        }
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("survey",survey);
+        resultMap.put("questions",questions);
+        return Result.success(resultMap);
+    }
+
+
+    @PostMapping("/saveBuild")
+    public Result<Void> saveSurvey(@RequestBody SurveyDto dto) {
+        System.out.println("survey: "+ dto.getSurvey());
+
+        Survey survey = dto.getSurvey();
+        List<Question> questions= dto.getQuestions();
+        if(survey.getSurveyId()!=0){
+            surveyService.updateSurvey(survey);
+            handleSurveyContent(questions,survey);
+        }else{
+            surveyService.createSurvey(survey);
+            System.out.println("surveyId: "+survey.getSurveyId());
+            for(Question question:questions){
+                System.out.println(question);
+            }
+            handleSurveyContent(questions,survey);
+        }
+        return Result.success();
+    }
+
+
+    @PostMapping("/updateBuild")
+    public Result<Void> updateBuildSurvey(@RequestBody SurveyDto dto) {
+        Survey survey = dto.getSurvey();
+        List<Question> questions=dto.getQuestions();
+        surveyService.updateSurvey(survey);
+        handleSurveyContent(questions,survey);
+        return Result.success();
+    }
+
+    public void handleSurveyContent(List<Question> questions,Survey survey){
+        System.out.println("handleSurveyContent survey: "+survey);
+        for(Question question:questions){
+
+            if(question.getQuestionId()!=0){
+                question.setSurveyId(survey.getSurveyId());
+                int questionId = question.getQuestionId();
+                questionService.updateQuestion(question);
+                for(Option option:question.getOptions()){
+                    if(option.getOptionId()!=0){
+                        option.setQuestionId(questionId);
+                        optionService.updateOption(option);
+                    }else {
+                        optionService.addOption(option);
+                    }
+                }
+            }else {
+                System.out.println("handleSurveyContent");
+                question.setSurveyId(survey.getSurveyId());
+                questionService.addQuestionAndReturnId(question);
+                int questionId =question.getQuestionId();
+                System.out.println("handleSurveyContent questionId: "+questionId);
+                System.out.println("handleSurveyContent surveyId: "+survey.getSurveyId());
+
+                for(Option option:question.getOptions()){
+                    option.setQuestionId(questionId);
+                    if(option.getOptionId()!=0){
+                        optionService.updateOption(option);
+                    }else {
+                        optionService.addOption(option);
+                    }
+                }
+            }
+        }
     }
 
     @PostMapping("/add")
---
+++ src/main/java/SurveySystem/Model/SurveyDto.java
@@ -0,0 +1,12 @@
+package SurveySystem.Model;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SurveyDto {
+    private Survey survey;
+    private List<Question> questions;
+}
